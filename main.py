f88cd9c9786813bb045c1fd9ab94223371cfc91e
ふぁ <yuki@yuki0311.com>
2024-05-08 18:46:28 +0900

    update
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import json
-from typing import Union 
 
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get("/favicon.ico")
-async def write_data():
+async def favicon():
     raise HTTPException(status_code=404)
+
 
 @app.get("/{key}")
 async def write_data(
+    request: Request,
     key: str,
-    user_agent: Union[str, None] = Header(default=None),
 ):
     with open("output.json", "r") as file:
         data = json.load(file)
-    data[key] = user_agent
+    data[key] = request.headers.get("user-agent")
     with open("output.json", "w") as file:
         json.dump(data, file, indent=4)
-    
+
+    with open("header.json", "r") as file:
+        data = json.load(file)
+    data[key] = {}
+    for header, value in request.headers.items():
+        data[key][header] = value
+    with open("header.json", "w") as file:
+        json.dump(data, file, indent=4)
+
     return data
+
+
+@app.get("/fetch/{key}")
+async def fetch_data(
+    key: str,
+):
+    with open("test.html", "r") as file:
+        data = file.read()
+    return HTMLResponse(
+        content=data.replace("{{BROWSER_CLIENT_KEY}}", key),
+        status_code=200,
+    )
